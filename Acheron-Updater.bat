085fdab88dc0fbef1fdc592962b77938ee59b891
ZenFyre <149545423+Zen-Fyre@users.noreply.github.com>
2026-08-06 14:41:09 -0300

    Update Acheron-Updater.bat
    Made the code readable with the power of slop

--- Acheron-Updater.bat
+++ Acheron-Updater.bat
@@ -1,3 +1,10 @@
 @echo off
-START "Acheron updater" /min cmd /c "curl -o Acheron.zip -LJO https://nightly.link/ouwou/acheron/workflows/build/master/acheron-windows-MinSizeRel.zip && 7z -y x Acheron.zip && DEL Acheron.Zip && Start Acheron.exe"
+
+START "Acheron updater" /min cmd /c ^
+"curl -o Acheron.zip -LJO https://nightly.link/ouwou/acheron/workflows/build/master/acheron-windows-MinSizeRel.zip ^
+& 7z -y x Acheron.zip ^
+& DEL Acheron.Zip ^
+& Start Acheron.exe ^
+& exit /b"
+
 exit /b
