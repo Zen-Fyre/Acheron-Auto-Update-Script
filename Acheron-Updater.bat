2e3afba8db4c3f2df0832eea72e32a06960fd80c
ZenFyre <149545423+Zen-Fyre@users.noreply.github.com>
2026-08-06 14:35:51 -0300

    Update Acheron-Updater.bat
    Doing it in the background and without powershell cuz it was too slow and annoying

--- Acheron-Updater.bat
+++ Acheron-Updater.bat
@@ -1,7 +1,3 @@
 @echo off
-POWERSHELL Invoke-WebRequest https://nightly.link/ouwou/acheron/workflows/build/master/acheron-windows-MinSizeRel.zip -OutFile Acheron.zip
-	7z x -y Acheron.zip
-	DEL Acheron.zip
-
-start "" "acheron.exe"
+START "Acheron updater" /min cmd /c "curl -o Acheron.zip -LJO https://nightly.link/ouwou/acheron/workflows/build/master/acheron-windows-MinSizeRel.zip && 7z -y x Acheron.zip && DEL Acheron.Zip && Start Acheron.exe"
 exit /b
