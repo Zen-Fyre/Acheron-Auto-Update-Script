10601852d9f9196027dbc6b3e4504d8675245cee
ZenFyre <149545423+Zen-Fyre@users.noreply.github.com>
2026-09-12 15:41:30 -0300

    Fix missing 7z ENV PATH and CURL download on Acheron-Updater.bat
    After reinstalling for the 7th time my SlopOS 10 this year, I decided to ditch Nanazip my 7z env path didn't exist.
    
    Also CURL was unhappy with some multiple path issue, used slop to help me out there.
    
     * Added a check for 7z usual path on %programfiles%, and detect if 7z ENV path was already set
     * Added a stopper when 7z path is not set to notify and not download Acheron unnecessarely
     * Fixed CURL having the output file name being set twice (apparently according to SLOP chat)

--- Acheron-Updater.bat
+++ Acheron-Updater.bat
@@ -1,10 +1,31 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-START "Acheron updater" /min cmd /c ^
-"curl -o Acheron.zip -LJO https://nightly.link/ouwou/acheron/workflows/build/master/acheron-windows-MinSizeRel.zip ^
-& 7z -y x Acheron.zip ^
+:: Configuring the path of 7z in case no 7z for is installed 
+::eg. NanaZip from v1 and stop unecessary download if not found
+
+SET "7zipDir="
+IF DEFINED 7z SET "7zipDir=%7z%"
+IF NOT DEFINED 7zipDir IF EXIST "%programfiles%\7-Zip" (SET "7zipDir=%programfiles%\7-Zip\7z.exe")
+IF NOT DEFINED 7zipDir call :7z_missing
+
+
+
+::Start download/Unzip on background
+
+START "Acheron updater" /MIN CMD /c ^
+"CURL -o Acheron.zip -L https://nightly.link/ouwou/acheron/workflows/build/master/acheron-windows-MinSizeRel.zip ^
+&"!7zipDir!" -y x Acheron.zip ^
 & DEL Acheron.Zip ^
-& Start Acheron.exe ^
-& exit /b"
+& START Acheron.exe ^
+& EXIT /b"
 
-exit /b
+EXIT /b
+
+
+:7z_missing
+ECHO Standard install of 7z detected, nor a ENV PATH is set
+ECHO Please set a ENV PATH of 7z[fork] or install 7z to get it located on "%programfiles%\7-Zip\7z.exe"
+ECHO.
+PAUSE
+EXIT /B
