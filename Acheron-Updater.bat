b700024be5a162d608ede31f5bf51b0150430ea0
ZenFyre <149545423+Zen-Fyre@users.noreply.github.com>
2026-09-19 16:54:48 -0300

    Update Acheron-Updater.bat
    Added check for blacklisted builds based off sha256 and not extract the broken build.
    Unfortunately could not find a way that with my skills allow to get the sha256 without downloading the whole archive, and the slop machine said so too, so it was useless this update

--- Acheron-Updater.bat
+++ Acheron-Updater.bat
@@ -1,4 +1,9 @@
 @echo off
+
+::start minimized from https://stackoverflow.com/a/22357573
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit 
+
+
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 :: Configuring the path of 7z in case no 7z for is installed 
@@ -10,16 +15,25 @@ IF NOT DEFINED 7zipDir IF EXIST "%programfiles%\7-Zip" (SET "7zipDir=%programfil
 IF NOT DEFINED 7zipDir call :7z_missing
 
 
+::Start download/Unzip
 
-::Start download/Unzip on background
+SET "BLACKLISTED=FALSE"
 
-START "Acheron updater" /MIN CMD /c ^
-"CURL -o Acheron.zip -L https://nightly.link/ouwou/acheron/workflows/build/master/acheron-windows-MinSizeRel.zip ^
-&"!7zipDir!" -y x Acheron.zip ^
-& DEL Acheron.Zip ^
-& START Acheron.exe ^
-& EXIT /b"
-
+CURL -o Acheron.zip -L https://nightly.link/ouwou/acheron/workflows/build/master/acheron-windows-MinSizeRel.zip
+FOR /F "SKIP=1 TOKENS=1" %%A IN ('CERTUTIL -HASHFILE "Acheron.zip" SHA256') DO IF NOT DEFINED HASH SET "HASH=%%A"
+CURL -o BLACKLIST.TXT -L "https://raw.githubusercontent.com/Zen-Fyre/Acheron-Auto-Update-Script/main/sha256-blacklist"
+FOR /F "SKIP=3 DELIMS=: TOKENS=2" %%A IN (BLACKLIST.TXT) DO (
+	IF /i "%HASH%"=="%%A" (
+		SET "BLACKLISTED=TRUE"
+		ECHO GOT BLACKLISTED
+		)
+	ECHO %HASH%
+	ECHO %%A
+	)
+IF !BLACKLISTED!==FALSE ("!7zipDir!" -y x Acheron.zip)
+DEL Acheron.Zip
+DEL BLACKLIST.TXT
+START Acheron.exe
 EXIT /b
 
 
@@ -28,4 +42,4 @@ ECHO Standard install of 7z detected, nor a ENV PATH is set
 ECHO Please set a ENV PATH of 7z[fork] or install 7z to get it located on "%programfiles%\7-Zip\7z.exe"
 ECHO.
 PAUSE
-EXIT /B
+EXIT
